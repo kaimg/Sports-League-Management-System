a006e55ecc70b81ad81012f2d56eee35fc0edcff
Henmart02 <00012622@uca.edu.sv>
2026-05-13 13:45:32 -0600

    Add GeoJSON filtering and enhanced stadium popups

--- gis_routes.py
+++ gis_routes.py
@@ -87,22 +87,26 @@ def api_nearby_stadiums():
 
     cur.execute("""
         SELECT
-            stadium_id,
-            name,
-            location,
-            capacity,
-            city,
-            country,
-            latitude,
-            longitude,
+            s.stadium_id,
+            s.name,
+            s.location,
+            s.capacity,
+            s.city,
+            s.country,
+            s.latitude,
+            s.longitude,
+            t.name AS team_name,
+            l.name AS league_name,
             ST_Distance(
-                geom::geography,
+                s.geom::geography,
                 ST_SetSRID(ST_MakePoint(%s, %s), 4326)::geography
             ) / 1000 AS distance_km
-        FROM stadiums
-        WHERE geom IS NOT NULL
+        FROM stadiums s
+        LEFT JOIN teams t ON t.stadium_id = s.stadium_id
+        LEFT JOIN leagues l ON t.league_id = l.league_id
+        WHERE s.geom IS NOT NULL
           AND ST_DWithin(
-                geom::geography,
+                s.geom::geography,
                 ST_SetSRID(ST_MakePoint(%s, %s), 4326)::geography,
                 %s
           )
@@ -118,19 +122,103 @@ def api_nearby_stadiums():
     stadiums = cur.fetchall()
     cur.close()
 
-    data = []
+    features = []
 
     for stadium in stadiums:
-        data.append({
-            "id": stadium[0],
-            "name": stadium[1],
-            "location": stadium[2],
-            "capacity": stadium[3],
-            "city": stadium[4],
-            "country": stadium[5],
-            "latitude": float(stadium[6]),
-            "longitude": float(stadium[7]),
-            "distance_km": round(stadium[8], 2)
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    float(stadium[7]),
+                    float(stadium[6])
+                ]
+            },
+            "properties": {
+                "id": stadium[0],
+                "name": stadium[1],
+                "location": stadium[2],
+                "capacity": stadium[3],
+                "city": stadium[4],
+                "country": stadium[5],
+                "team_name": stadium[8],
+                "league_name": stadium[9],
+                "distance_km": round(stadium[10], 2)
+            }
         })
 
-    return jsonify(data)
+    return jsonify({
+        "type": "FeatureCollection",
+        "features": features
+    })
+@gis_bp.route('/api/stadiums/geojson')
+def api_stadiums_geojson():
+    db = get_db()
+    cur = db.cursor()
+
+    city = request.args.get('city')
+    country = request.args.get('country')
+    league_id = request.args.get('league_id')
+
+    query = """
+        SELECT DISTINCT
+            s.stadium_id,
+            s.name,
+            s.location,
+            s.capacity,
+            s.city,
+            s.country,
+            s.latitude,
+            s.longitude,
+            t.name AS team_name,
+            l.name AS league_name
+        FROM stadiums s
+        LEFT JOIN teams t ON t.stadium_id = s.stadium_id
+        LEFT JOIN leagues l ON t.league_id = l.league_id
+        WHERE s.latitude IS NOT NULL
+          AND s.longitude IS NOT NULL
+    """
+
+    params = []
+
+    if city:
+        query += " AND LOWER(s.city) LIKE LOWER(%s)"
+        params.append(f"%{city}%")
+
+    if country:
+        query += " AND LOWER(s.country) LIKE LOWER(%s)"
+        params.append(f"%{country}%")
+
+    if league_id:
+        query += " AND t.league_id = %s"
+        params.append(league_id)
+
+    cur.execute(query, params)
+    stadiums = cur.fetchall()
+    cur.close()
+
+    features = []
+
+    for stadium in stadiums:
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [float(stadium[7]), float(stadium[6])]
+            },
+            "properties": {
+                "id": stadium[0],
+                "name": stadium[1],
+                "location": stadium[2],
+                "capacity": stadium[3],
+                "city": stadium[4],
+                "country": stadium[5],
+                "team_name": stadium[8],
+                "league_name": stadium[9]
+            }
+        })
+
+    return jsonify({
+        "type": "FeatureCollection",
+        "features": features
+    })
